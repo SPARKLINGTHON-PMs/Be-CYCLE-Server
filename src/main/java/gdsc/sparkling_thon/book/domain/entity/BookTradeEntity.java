0febd548d9cfd612dd0d425b322918a5ec933000
Minju-Kimm <alswn299842@naver.com>
2024-08-25 05:09:32 +0900

    feat: 책 거래 관련 API 구현 완료

--- src/main/java/gdsc/sparkling_thon/book/domain/entity/BookTradeEntity.java
+++ src/main/java/gdsc/sparkling_thon/book/domain/entity/BookTradeEntity.java
@@ -3,6 +3,7 @@ package gdsc.sparkling_thon.book.domain.entity;
 import gdsc.sparkling_thon.book.domain.enums.TradeStateEnum;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -36,4 +37,18 @@ public class BookTradeEntity {
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @Builder
+    public BookTradeEntity(BookEntity buyBook, BookEntity sellBook, TradeStateEnum status) {
+        this.buyBook = buyBook;
+        this.sellBook = sellBook;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setStatus(TradeStateEnum status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
