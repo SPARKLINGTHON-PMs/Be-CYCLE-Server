1c55147e4f58df5709382788ba958f6ee3d34cba
임채성 <75980875+puleugo@users.noreply.github.com>
2024-08-25 04:20:44 +0900

    feat: OCR 유틸리티 구현  (#5)
    * chore: 구글 비전 라이브러리 추가
    
    * feat: 이미지 유틸리티 추가
    
    * feat: OCR API 호출 클래스 구현
    
    * refactor: 메서드 재명명 및 정적 메서드로 변경

---
+++ src/main/java/gdsc/sparkling_thon/util/ImageUtil.java
@@ -0,0 +1,19 @@
+package gdsc.sparkling_thon.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class ImageUtil {
+    public static String toByteString(MultipartFile file) {
+        try {
+            return Base64.getEncoder().encodeToString(file.getBytes()).replaceAll("\n", "");
+        } catch (IOException e) {
+            System.out.println("이미지 리사이즈에 실패했습니다.");
+        }
+        return null;
+    }
+}
